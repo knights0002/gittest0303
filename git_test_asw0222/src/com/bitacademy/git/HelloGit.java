c27e8f4e165be22dcafd4c60d11ce00070660368
MK2 <knights0002@naver.com>
2017-03-03 11:26:45 +0900

    프로젝트 공유

---
+++ git_test_asw0222/src/com/bitacademy/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.bitacademy.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("안녕 GIT");
+		System.out.println("앞으로 친하게 지내자");
+
+	}
+
+}
